8f43c1f981223f1abd0f50c5d74340f0779edc3e
Rohan Zalani <rohanzalani@gmail.com>
2014-03-19 23:06:45 -0500

    Beat 5

--- 5.js
+++ 5.js
@@ -2,7 +2,12 @@ var fs = require('fs');
 var path = require('path');
 var dir = process.argv[2];
 var ext = process.argv[3];
+function callback(err,list){
+	for(var x=0; x<list.length; ++x){
+		if(path.extname(list[x])==='.' + ext){
+			console.log(list[x]);
+		}
+	}
+};
 
-function dirCallback(err,list){
-console.log(list[x].);
-fs.readdir(dir,dirCallback);
+fs.readdir(dir, callback );
